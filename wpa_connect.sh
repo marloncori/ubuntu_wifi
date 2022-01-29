6ff7075575b0dc207e1b16c5aec940d6ee1c717f
Marlon Couto Ribeiro <84450525+marloncori@users.noreply.github.com>
2022-01-29 21:42:26 +0100

    Update wpa_connect.sh

--- wpa_connect.sh
+++ wpa_connect.sh
@@ -4,11 +4,11 @@ wpa_passphrase "TP-Link_950F" "11070368" | sudo tee /etc/wpa_supplicant.conf
 
 sudo wpa_supplicant -B -c /etc/wpa_supplicant.conf -i wlan0
 
+sudo dhclient wlan0
+
 sudo systemctl stop NetworkManager
 
 sudo systemctl disable NetworkManager-wait-online NetworkManager-dispatcher NetworkManager
-
-sudo dhclient wlan0
 
 sudo cp /lib/systemd/system/wpa_supplicant.service /etc/systemd/system/wpa_supplicant.service
 
